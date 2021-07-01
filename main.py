3b438e0bdad41fca86efbe56e1d96f4908e9dbc1
hrishikeshp7 <68657160+hrishikeshp7@users.noreply.github.com>
2021-07-01 21:46:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+from cryptography.fernet import Fernet
+
+print("Enter the Text which you want to Encrypt")
+message = input()
+
+
+
+key = Fernet.generate_key()
+
+
+
+fernet = Fernet(key)
+
+
+encMessage = fernet.encrypt(message.encode())
+
+print("original string was : " , message)
+print("encrypted string is : " , encMessage)
+
+
+
+print("Press Any key to Exit")
+stop = input()
